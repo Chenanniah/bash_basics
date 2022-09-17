94a4fc9d46ecd39cb63de4eb6ac23891cb137618
chenanniah <cmaccephas@gmail.com>
2022-09-17 11:14:12 -0400

    Updated file with morning or not script

--- 04-life.sh
+++ 04-life.sh
@@ -4,9 +4,9 @@ read meaning
 
 
 if [ "$meaning" -eq 42 ]; then
-   echo "Yes!, That is the meaning of life!"
+   echo "Yes! That is the meaning of life!"
 else
-   echo "Awww... You don't know the meaning of life"
+   echo "Awww...You don't know the meaning of life"
 fi
 
 #  here are some other arithemetic comparison operators
@@ -14,3 +14,18 @@ fi
 
 # exercise: write a script that prints whether it is 
 # morning or not
+
+h=$(date +"%H")
+
+if [ $h -gt 6 -a $h -le 12 ]
+then
+echo It is morning
+elif [ $h -gt 12 -a $h -le 16 ]
+then 
+echo It is afternoon
+elif [ $h -gt 16 -a $h -le 20 ]
+then
+echo It is evening
+else
+echo It is night
+fi
